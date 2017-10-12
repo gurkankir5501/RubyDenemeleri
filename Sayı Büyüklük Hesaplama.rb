fc2ee1c277f2b868ba7a1e044234561a76da2986
Gürkan KIR <32749875+gurkankir5501@users.noreply.github.com>
2017-10-13 01:12:46 +0300

    Sayı Büyüklük Hesaplama

---
+++ Sayı Büyüklük Hesaplama.rb
@@ -0,0 +1,21 @@
+print " sayi1 giriniz :"
+
+sayi1 = gets.chomp.to_i
+
+print " sayi2 giriniz :"
+
+sayi2 = gets.chomp.to_i
+
+if
+
+ sayi1 > sayi2
+
+print " sayi1 büyüktür "
+
+else
+
+sayi1 < sayi2
+
+print " sayi2 büyüktür"
+
+end
