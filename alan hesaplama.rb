d28fa9216417619c3c13efe7dd45205dbaab3f41
Gürkan KIR <32749875+gurkankir5501@users.noreply.github.com>
2017-10-13 01:02:00 +0300

    Alan Hesaplama

---
+++ alan hesaplama.rb
@@ -0,0 +1,10 @@
+  puts " alan hesaplama "
+  
+  print "uzunluk giriniz : "
+uzunluk = gets.chomp.to_i
+  print " eni giriniz : "
+eni = gets.chomp.to_i
+
+alan = uzunluk * eni
+print " alan : "
+print alan
